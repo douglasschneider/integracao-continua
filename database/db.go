7ca0940928ec333081d2d0f75b3040ff09b7a9f2
Douglas Schneider <dlschneider@proton.me>
2024-10-06 17:08:24 -0300

    Alterado host do banco de dados

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	host := os.Getenv("localhost")
+	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	database := os.Getenv("DB_NAME")
